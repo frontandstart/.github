4d72f4018d3c976b516d1f117b5f088d8fc63470
Roman K <frontandstart@gmail.com>
2024-08-18 17:01:11 +0300

    Resolve host based auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.hosts << 'frontandstart.co'
-  config.hosts << 'frontandstart:3000, frontandstart.co'
+  config.hosts = ['frontandstart.co', 'frontandstart:3000']
 end
