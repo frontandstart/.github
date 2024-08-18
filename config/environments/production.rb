259a8f05838a35aa36e97679c9293ec59e87cd78
Roman K <frontandstart@gmail.com>
2024-08-18 16:45:33 +0300

    Allow production hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << 'frontandstart.co'
+  config.hosts << 'frontandstart:3000'
 end
